18abccfd186f69ff759e3d256eae7b4559a16a6c
J. Paulo Seibt <jpseibt@gmail.com>
2025-12-14 18:32:37 -0300

    Add l5/kr3-1_expand exercise

---
+++ l5/kr3-1_expand.c
@@ -0,0 +1,47 @@
+/* Exercise 3-1. Write a function expand(s, t) which converts characters like newline and tab
+   into visible escape sequences like \n and \t as it copies the string s to t. Use a switch.
+*/
+
+#include <stdio.h>
+
+#define SIZE 255
+void expand(char *s, char *t, int len);
+
+int main()
+{
+    char s[SIZE] = "abc\tdefg\nhi\tjk\tl\nmnop\n";
+    char t[SIZE];
+    expand(s, t, SIZE);
+    printf("s = \"%s\"\nt = \"%s\"\n", s, t);
+    return 0;
+}
+
+void expand(char *s, char *t, int len)
+{
+    int i, j, c;
+    i = j = 0;
+
+    // Check if there's space to right 2 chars
+    for (c = s[0]; c != '\0' && j < len - 2; c = s[++i], ++j)
+    {
+        switch (c)
+        {
+            case '\n':
+                t[j] = '\\';
+                t[++j] = 'n';
+                break;
+            case '\t':
+                t[j] = '\\';
+                t[++j] = 't';
+                break;
+            default:
+                t[j] = c;
+                break;
+        }
+    }
+
+    if (c != '\0' && j < len - 1 && c != '\n' && c != '\t')
+        t[j++] = c;
+
+    t[j] = '\0';
+}
